09b00fe41ab4d2a25f6de1f2b70795ec0553edda
amir-kamel <amir.kghabrial@gmail.com>
2025-05-04 22:29:39 +0300

    fixing adding page of login for login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -39,7 +39,7 @@ function Login() {
           <input
             type="text"
             className="form-control"
-            placeholder="Username"
+            placeholder="admin"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
@@ -47,7 +47,7 @@ function Login() {
           <input
             type="password"
             className="form-control"
-            placeholder="Password"
+            placeholder="admin"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
