c3c188a72479468ee0d8bba37c13db435ababaf1
amir-kamel <amir.kghabrial@gmail.com>
2025-05-09 00:25:22 +0300

    fixing photos url

--- src/config/axios_conf.js
+++ src/config/axios_conf.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Dynamically set the base URL based on the environment
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? "https://hotels-reservation-system-api.onrender.com" 
+  ? "https://hotelsreservationsystemapi-production.up.railway.app" 
   : "http://127.0.0.1:8000";
 
 const axiosInstance = axios.create({
